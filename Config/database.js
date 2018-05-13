bcbc498e20242018a27c4aeaaae15b8801b9a16c
Escandon <aleskndon@hotmail.com>
2018-05-13 17:17:23 -0500

    Delete database.js
    because is my database bitch

--- Config/database.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    AUTH0_DOMAIN: 'ds123399.mlab.comds123399.mlab.com',
-    API: 'http://127.0.0.1:3000/',
-    secret:'aseguradora',
-    MONGO_URI: 'mongodb://Xnecro:N0paginaweb@ds123399.mlab.com:23399/mean'
-};
